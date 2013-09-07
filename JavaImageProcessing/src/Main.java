8caaba469b0cc5a3cd55157f298cdb648a5cd397
demidrol911 <dmitrii_911@mail.ru>
2013-09-07 21:11:16 +1000

    Update Main.java

--- JavaImageProcessing/src/Main.java
+++ JavaImageProcessing/src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class Main {
    
    public static void main(String[] args){
-	  JFrame.setDefaultLookAndFeelDecorated(true);
+      JFrame.setDefaultLookAndFeelDecorated(true);
       AppWindow app = new AppWindow();
       app.setVisible(true);
    }
