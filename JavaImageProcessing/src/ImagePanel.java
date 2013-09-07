d23d35382d51afe789bc7dc2da3d1f747b6225ba
demidrol911 <dmitrii_911@mail.ru>
2013-09-07 21:12:53 +1000

    Update ImagePanel.java

--- JavaImageProcessing/src/ImagePanel.java
+++ JavaImageProcessing/src/ImagePanel.java
@@ -24,21 +24,21 @@ public class ImagePanel extends JComponent {
    }
    
    public void paint(Graphics g){
-	  super.paint(g);
-	  Graphics2D g2 = (Graphics2D)g;
-	  g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-	  g2.setColor(Color.GRAY);
-	  g2.fillRect(10, 20, this.getWidth()-20, this.getHeight()-30);
-	  if(image == null) {
-		 String message  = "Please upload your image :)";
-		 g2.setColor(Color.BLACK);
-		 g2.setFont(new Font(g2.getFont().getFamily(), Font.BOLD, 20));
-		 FontMetrics fm = g2.getFontMetrics();
+      super.paint(g);
+      Graphics2D g2 = (Graphics2D)g;
+      g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+      g2.setColor(Color.GRAY);
+      g2.fillRect(10, 20, this.getWidth()-20, this.getHeight()-30);
+      if(image == null) {
+         String message  = "Please upload your image :)";
+         g2.setColor(Color.BLACK);
+         g2.setFont(new Font(g2.getFont().getFamily(), Font.BOLD, 20));
+	 FontMetrics fm = g2.getFontMetrics();
          Rectangle2D r = fm.getStringBounds(message, g2);
          int x = (this.getWidth() - (int) r.getWidth())/2;
          int y = (this.getHeight() + 10 - (int) r.getHeight())/2 + fm.getAscent();
          g2.drawString(message, x, y);
-	  }
+      }
    }
    
    private BufferedImage image;
